5807b53d179b165d2cc4c0fee80a5e93f4363944
Dima <dchaika1990@gmail.com>
2021-09-14 18:51:25 +0300

    add select

--- src/app/modules/root/components/root/root.component.ts
+++ src/app/modules/root/components/root/root.component.ts
@@ -17,28 +17,26 @@ export class RootComponent implements OnInit {
 	isNew: Boolean = true
 	sortField: string = 'name'
 	sortFlag: any = true
+	types: string[] = ['truck', 'trailer', 'transport']
 
 	constructor(private modalService: NgbModal) {
 	}
 
 	ngOnInit(): void {
 		this.assetList.push(
-			{id: 1, name: 'Name 1', type: 'Track 1', latitude: 20, longitude: 10},
-			{id: 2, name: 'Name 2', type: 'Brack 2', latitude: 15, longitude: 20},
+			{id: 1, name: 'Name 1', type: 'truck', latitude: 20, longitude: 10},
+			{id: 2, name: 'Name 2', type: 'trailer', latitude: 15, longitude: 20},
 		)
 	}
 
 	open(context: any, el: any, isNew: boolean = true) {
 		this.isNew = isNew
 		this.modalEl = {...el}
-		this.modalService.open(context, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
-
-		}, (reason) => {
-
-		});
+		this.modalService.open(context, {ariaLabelledBy: 'modal-basic-title'})
 	}
 
 	save(modal: any) {
+		console.log(this.modalEl)
 		let idx = this.assetList.findIndex(elem => elem.id === this.modalEl.id)
 		if (idx < 0) {
 			this.modalEl.id = Date.now()
@@ -48,7 +46,6 @@ export class RootComponent implements OnInit {
 			this.currentAsset = {...this.modalEl}
 		}
 		modal.close()
-		console.log(this.assetList)
 	}
 
 	delete(el: Asset) {
